03dda5a36f71fb165771f142a5afbd2da24b7998
Waksim <43213750+Waksim@users.noreply.github.com>
2024-12-19 00:26:22 +0300

    Delete MKvoice directory

--- MKvoice/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-LOG_FILE = os.getenv("LOG_FILE", "bot_log.log")
-AUDIO_FOLDER = os.getenv("AUDIO_FOLDER", "audio")
